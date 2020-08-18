939b1d1be424d2295d40bee4c5aedebf12e6be49
MiguelIsaza95 <miguel11andres@hotmail.com>
2020-08-18 09:16:37 -0500

    Add az to elb

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "elb_laravel" {
   name    = "laravel-elb"
-  subnets = aws_subnet.dmz_public.*.id
+  subnets = [aws_subnet.dmz_public.0.id, aws_subnet.dmz_public.1.id, aws_subnet.front_private.2.id]
   # subnets helps to attach the lb to your current vpc
   # we can define az or subnets, but just one of them.
   security_groups = [aws_security_group.elb_laravel_sg.id]
